43df3b49a0732987ff0b96d90820b4fec09447c9
Meghna <meghnatejaswani10@gmail.com>
2026-09-01 12:32:23 +0530

    Prepare Flask app for Vercel

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Welcome to Flask!"
+
+@app.route("/about")
+def about():
+    return "This is the About page."
